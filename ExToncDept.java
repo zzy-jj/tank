d44a4b6e3fefc975270ad3348f1c24ac2e18e224
zzy-jj <52873066+zzy-jj@users.noreply.github.com>
2021-11-16 16:30:14 +0800

    Add files via upload
    测试Git上传

---
+++ ExToncDept.java
@@ -0,0 +1,93 @@
+
+package service;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementRef;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>anonymous complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="string" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="string1" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "string",
+    "string1"
+})
+@XmlRootElement(name = "exToncDept")
+public class ExToncDept {
+
+    @XmlElementRef(name = "string", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> string;
+    @XmlElementRef(name = "string1", type = JAXBElement.class, required = false)
+    protected JAXBElement<String> string1;
+
+    /**
+     * 获取string属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getString() {
+        return string;
+    }
+
+    /**
+     * 设置string属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setString(JAXBElement<String> value) {
+        this.string = value;
+    }
+
+    /**
+     * 获取string1属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public JAXBElement<String> getString1() {
+        return string1;
+    }
+
+    /**
+     * 设置string1属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link JAXBElement }{@code <}{@link String }{@code >}
+     *     
+     */
+    public void setString1(JAXBElement<String> value) {
+        this.string1 = value;
+    }
+
+}
